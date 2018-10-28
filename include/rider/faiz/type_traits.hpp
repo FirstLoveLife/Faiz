687bc74cac7d810ad4ef4e90b0041cbc5b8fbf44
firstlove <c941968215@gmail.com>
2018-10-28 10:09:50 +0800

    add traits

--- include/rider/faiz/type_traits.hpp
+++ include/rider/faiz/type_traits.hpp
@@ -145,7 +145,6 @@ namespace rider
 
         // specializations for noexcept versions of all the above (C++17 and
         // later)
-
         template<class Ret, class... Args>
         struct is_function<Ret(Args...) noexcept> : true_type
         {
@@ -248,7 +247,10 @@ namespace rider
 
         /******************** remove *********************/
 
-        //
+
+        // If the type T is a reference type,
+        // provides the member typedef type which is the type referred to by T.
+        // Otherwise type is T.
         template<class T>
         struct remove_reference
         {
@@ -264,6 +266,8 @@ namespace rider
         {
             using type = T;
         };
+        template<class T>
+        using remove_reference_t = typename remove_reference<T>::type;
 
         template<class T>
         struct remove_const
@@ -287,11 +291,65 @@ namespace rider
             using type = T;
         };
 
+        // If T is an array of some type X, provides the member typedef type equal to X, otherwise type is T.
+        // Note that if T is a multidimensional array, only the first dimension is removed.
+        template<class T>
+        struct remove_extent
+        {
+            using type = T;
+        };
+
+        template<class T>
+        struct remove_extent<T[]>
+        {
+            using type = T;
+        };
+
+        template<class T, size_t N>
+        struct remove_extent<T[N]>
+        {
+            using type = T;
+        };
+
         template<class T>
         struct remove_cv
         {
             using type = typename remove_volatile<typename remove_const<T>::type>::type;
         };
+
+
+        // Provides the member typedef type which is the type pointed to by T,
+        // or, if T is not a pointer, then type is the same as T.
+        // ```cpp
+        // print_is_same<int, remove_pointer<int*>::type>();  // true
+        // print_is_same<int, remove_pointer<int**>::type>(); // false
+        // ```
+        template<class T>
+        struct remove_pointer
+        {
+            using type = T;
+        };
+        template<class T>
+        struct remove_pointer<T*>
+        {
+            using type = T;
+        };
+        template<class T>
+        struct remove_pointer<T* const>
+        {
+            using type = T;
+        };
+        template<class T>
+        struct remove_pointer<T* volatile>
+        {
+            using type = T;
+        };
+        template<class T>
+        struct remove_pointer<T* const volatile>
+        {
+            using type = T;
+        };
+
 
         template<class T>
         using remove_cv_t = typename remove_cv<T>::type;
@@ -299,16 +357,35 @@ namespace rider
         using remove_const_t = typename remove_const<T>::type;
         template<class T>
         using remove_volatile_t = typename remove_volatile<T>::type;
+        template<class T>
+        using remove_extent_t = typename remove_extent<T>::type;
+        template<class T>
+        using remove_pointer_t = typename remove_pointer<T>::type;
 
+        // If the type **T** is a reference type, provides the member typedef type which is the type referred to by
+        // **T** with its topmost cv-qualifiers removed. Otherwise type is **T** with its topmost cv-qualifiers removed.
+        //
+        // Removing cvref does most of what decay does, but doesn't convert function types and array types to pointers.
+
+        template<class T>
+        struct remove_cvref
+        {
+            using type = remove_cv_t<remove_reference_t<T>>;
+        };
+        template<class T>
+        using remove_cvref_t = typename remove_cvref<T>::type;
 
         /******************** is *********************/
 
-        //
+        // If T and U name the same type (including const/volatile qualifications), provides the member constant value
+        // equal to true. Otherwise value is false.
         template<class T, class U>
         struct is_same : false_type
         {
         };
 
+        // If T and U name the same type (including const/volatile qualifications), provides the member constant value
+        // equal to true. Otherwise value is false.
         template<class T>
         struct is_same<T, T> : true_type
         {
@@ -404,6 +481,31 @@ namespace rider
         };
 
 
+        template<class T, unsigned N = 0>
+        struct extent : integral_constant<size_t, 0>
+        {
+        };
+
+        template<class T>
+        struct extent<T[], 0> : integral_constant<size_t, 0>
+        {
+        };
+
+        template<class T, unsigned N>
+        struct extent<T[], N> : extent<T, N - 1>
+        {
+        };
+
+        template<class T, size_t I>
+        struct extent<T[I], 0> : integral_constant<size_t, I>
+        {
+        };
+
+        template<class T, size_t I, unsigned N>
+        struct extent<T[I], N> : extent<T, N - 1>
+        {
+        };
+
         template<typename T, bool = is_referenceable<T>::value>
         struct add_lvalue_reference
         {
@@ -444,7 +546,7 @@ namespace rider
         template<class T>
         struct enable_if<true, T>
         {
-            typedef T type;
+            using type = T;
         };
 
         template<class T>
@@ -480,18 +582,139 @@ namespace rider
             using type = decltype(test<F, T>(0));
         };
 
-        template<typename T, typename U>
-        struct is_swappable_with
-            : public integral_constant<bool, is_convertible<U, T>::value && is_convertible<T, U>::value>
+
+        // Provides member typedef type, which is defined as T if B is true at compile time, or as F if B is false.
+        template<bool B, class T, class F>
+        struct conditional
         {
-        };
-        template<typename T, typename U>
-        struct is_nothrow_swappable_with
-            : public integral_constant<bool, is_convertible<U, T>::value && is_convertible<T, U>::value>
-        {
+            using type = T;
         };
 
+        // Provides member typedef type, which is defined as T if B is true at compile time, or as F if B is false.
+        template<class T, class F>
+        struct conditional<false, T, F>
+        {
+            using type = F;
+        };
 
+        //  Provides member typedef type, which is defined as T if B is true at compile time, or as F if B is false.
+        template<bool B, class T, class F>
+        using conditional_t = typename conditional<B, T, F>::type;
+
+        // If T is an array type, provides the member constant value equal to the number of dimensions of the array.
+        // For any other type, value is 0.
+        template<class T>
+        struct rank : public integral_constant<size_t, 0>
+        {
+        };
+        // If T is an array type, provides the member constant value equal to the number of dimensions of the array.
+        // For any other type, value is 0.
+        template<class T>
+        struct rank<T[]> : public integral_constant<size_t, rank<T>::value + 1>
+        {
+        };
+        // If T is an array type, provides the member constant value equal to the number of dimensions of the array.
+        // For any other type, value is 0.
+        template<class T, size_t N>
+        struct rank<T[N]> : public integral_constant<size_t, rank<T>::value + 1>
+        {
+        };
+        template<class T>
+        inline constexpr size_t rank_v = rank<T>::value;
+
+
+        namespace detail
+        {
+            template<class T, bool is_function_type = false>
+            struct add_pointer
+            {
+                using type = typename remove_reference<T>::type*;
+            };
+
+            template<class T>
+            struct add_pointer<T, true>
+            {
+                using type = T;
+            };
+
+            template<class T, class... Args>
+            struct add_pointer<T(Args...), true>
+            {
+                using type = T (*)(Args...);
+            };
+
+            template<class T, class... Args>
+            struct add_pointer<T(Args..., ...), true>
+            {
+                using type = T (*)(Args..., ...);
+            };
+
+        } // namespace detail
+
+        // If **T** is a reference type, then provides the member typedef type which is a pointer to the referred type.
+        // Otherwise, if **T** names an object type, a function type that is not **cv-** or **ref-qualified** (since
+        // C++17), or a (possibly **cv-qualified**) void type, provides the member typedef type which is the type T*.
+        // Otherwise (if T is a **cv-** or **ref-qualified** function type), provides the member typedef type which is
+        // the type **T**
+        //
+        // * Name:	Definition
+        // * type:	pointer to T or to the type referenced by T
+        template<class T>
+        struct add_pointer : detail::add_pointer<T, is_function<T>::value>
+        {
+        };
+        template<class T>
+        using add_pointer_t = typename add_pointer<T>::type;
+
+        // Applies lvalue-to-rvalue, array-to-pointer,
+        // and function-to-pointer implicit conversions to the type **T**,
+        // removes cv-qualifiers, and defines the resulting type as the member typedef type. Formally:
+        // * If **T** names the type "**array of U**" or "**reference to array of U**", the member typedef type is
+        // **U***.
+        // * Otherwise, if **T** is a function type **F** or a reference thereto, the member typedef type is
+        // **add_pointer<F>::type**.
+        // * Otherwise, the member typedef type is **remove_cv<remove_reference<T>::type>::type**
+        //
+        //```cpp
+        // #include <iostream>
+        // #include <type_traits>
+        // template <typename T, typename U>
+        // struct decay_equiv :
+        //     is_same<typename std::decay<T>::type, U>::type
+        // {};
+        // int main()
+        // {
+        //     std::cout << std::boolalpha
+        //               << decay_equiv<int, int>::value << '\n'
+        //               << decay_equiv<int&, int>::value << '\n'
+        //               << decay_equiv<int&&, int>::value << '\n'
+        //               << decay_equiv<const int&, int>::value << '\n'
+        //               << decay_equiv<int[2], int*>::value << '\n'
+        //               << decay_equiv<int(int), int(*)(int)>::value << '\n';
+        // }
+        // ```
+        // true
+        // true
+        // true
+        // true
+        // true
+        // true
+        //
+        template<class T>
+        struct decay
+        {
+        private:
+            using U = typename remove_reference<T>::type;
+
+        public:
+            using type = typename conditional<is_array<U>::value,
+                typename remove_extent<U>::type*,
+                typename conditional<is_function<U>::value,
+                    typename add_pointer<U>::type,
+                    typename remove_cv<U>::type>::type>::type;
+        };
+        template<class T>
+        using decay_t = typename decay<T>::type;
 #    pragma clang diagnostic pop
     } // namespace faiz
 } // namespace rider
