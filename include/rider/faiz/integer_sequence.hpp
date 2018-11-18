8141c7b4958984c4396713368fd463ed90e7e75b
firstlove <c941968215@gmail.com>
2018-11-18 13:22:38 +0800

    improve format

--- include/rider/faiz/integer_sequence.hpp
+++ include/rider/faiz/integer_sequence.hpp
@@ -9,7 +9,7 @@ integer_sequence
 https://stackoverflow.com/questions/53350706/how-to-understand-libcxxs-implementation-of-make-integer-sequence
  */
 
-namespace rider::faiz::unroll
+namespace rider::faiz
 {
 	template<class Tindex, Tindex... V>
 	struct integer_sequence_aux
@@ -31,42 +31,18 @@ namespace rider::faiz::unroll
 				Tp...,
 				sizeof...(Tp) + Tp...,
 #define Aux(N)  N * sizeof...(Tp) + Tp...
-				Aux(  2 ),
-				Aux(  3 ),
-				Aux(  4 ),
-				Aux(  5 ),
-				Aux(  6 ),
-				Aux(  7 ),
-				Aux(  8 ),
-				Aux(  9 ),
-				Aux( 10 ),
-				Aux( 11 ),
-				Aux( 12 ),
-				Aux( 13 ),
-				Aux( 14 ),
-				Aux( 15 ),
-				Aux( 16 ),
-				Aux( 17 ),
-				Aux( 18 ),
-				Aux( 19 ),
-				Aux( 20 ),
-				Aux( 21 ),
-				Aux( 22 ),
-				Aux( 23 ),
-				Aux( 24 ),
-				Aux( 25 ),
-				Aux( 26 ),
-				Aux( 27 ),
-				Aux( 28 ),
-				Aux( 29 ),
-				Aux( 30 ),
-				Aux( 31 ),
-				Aux( 32 ),
-				Aux( 33 ),
-				Aux( 34 ),
-				Aux( 35 ),
-				Aux( 36 ),
-				Aux( 37 ),
+				Aux(  2 ), Aux(  3 ), Aux(  4 ),
+				Aux(  5 ), Aux(  6 ), Aux(  7 ),
+				Aux(  8 ), Aux(  9 ), Aux( 10 ),
+				Aux( 11 ), Aux( 12 ), Aux( 13 ),
+				Aux( 14 ), Aux( 15 ), Aux( 16 ),
+				Aux( 17 ), Aux( 18 ), Aux( 19 ),
+				Aux( 20 ), Aux( 21 ), Aux( 22 ),
+				Aux( 23 ), Aux( 24 ), Aux( 25 ),
+				Aux( 26 ), Aux( 27 ), Aux( 28 ),
+				Aux( 29 ), Aux( 30 ), Aux( 31 ),
+				Aux( 32 ), Aux( 33 ), Aux( 34 ),
+				Aux( 35 ), Aux( 36 ), Aux( 37 ),
 				Aux( 38 ),
 				Vextra...>;
 #undef Aux
@@ -109,44 +85,25 @@ namespace rider::faiz::unroll
 	MAKE(N, ImplMake(N))
 #define MultipleParityAndMake(N)
 		// clang-format off
-		ParityAndMake(  1 )
-		ParityAndMake(  2 )
-		ParityAndMake(  3 )
-		ParityAndMake(  4 )
-		ParityAndMake(  5 )
-		ParityAndMake(  6 )
-		ParityAndMake(  7 )
-		ParityAndMake(  8 )
-		ParityAndMake(  9 )
-		ParityAndMake( 10 )
-		ParityAndMake( 11 )
-		ParityAndMake( 12 )
-		ParityAndMake( 13 )
-		ParityAndMake( 14 )
-		ParityAndMake( 15 )
-		ParityAndMake( 16 )
-		ParityAndMake( 17 )
-		ParityAndMake( 18 )
-		ParityAndMake( 19 )
-		ParityAndMake( 20 )
-		ParityAndMake( 21 )
-		ParityAndMake( 22 )
-		ParityAndMake( 23 )
-		ParityAndMake( 24 )
-		ParityAndMake( 25 )
-		ParityAndMake( 26 )
-		ParityAndMake( 27 )
-		ParityAndMake( 28 )
-		ParityAndMake( 29 )
-		ParityAndMake( 30 )
-		ParityAndMake( 31 )
-		ParityAndMake( 32 )
-		ParityAndMake( 33 )
-		ParityAndMake( 34 )
-		ParityAndMake( 35 )
-		ParityAndMake( 36 )
-		ParityAndMake( 37 )
-		ParityAndMake( 38 )
+		ParityAndMake(  1 ) ParityAndMake(  2 )
+		ParityAndMake(  3 ) ParityAndMake(  4 )
+		ParityAndMake(  5 ) ParityAndMake(  6 )
+		ParityAndMake(  7 ) ParityAndMake(  8 )
+		ParityAndMake(  9 ) ParityAndMake( 10 )
+		ParityAndMake( 11 ) ParityAndMake( 12 )
+		ParityAndMake( 13 ) ParityAndMake( 14 )
+		ParityAndMake( 15 ) ParityAndMake( 16 )
+		ParityAndMake( 17 ) ParityAndMake( 18 )
+		ParityAndMake( 19 ) ParityAndMake( 20 )
+		ParityAndMake( 21 ) ParityAndMake( 22 )
+		ParityAndMake( 23 ) ParityAndMake( 24 )
+		ParityAndMake( 25 ) ParityAndMake( 26 )
+		ParityAndMake( 27 ) ParityAndMake( 28 )
+		ParityAndMake( 29 ) ParityAndMake( 30 )
+		ParityAndMake( 31 ) ParityAndMake( 32 )
+		ParityAndMake( 33 ) ParityAndMake( 34 )
+		ParityAndMake( 35 ) ParityAndMake( 36 )
+		ParityAndMake( 37 ) ParityAndMake( 38 )
 		ParityAndMake( 39 )
 		// clang-format on
 
@@ -175,6 +132,8 @@ namespace rider::faiz::unroll
 
 	template<class T, T _Ep>
 	struct make_integer_sequence_checked
+		: type_identity<
+			  make_integer_sequence_aux_unchecked<T, 0 <= _Ep ? _Ep : 0>>
 	{
 		static_assert(faiz::is_integral<T>::value,
 			"std::make_integer_sequence can only be instantiated with an "
@@ -182,9 +141,6 @@ namespace rider::faiz::unroll
 		static_assert(0 <= _Ep,
 			"std::make_integer_sequence must have a non-negative sequence "
 			"length");
-		// Workaround GCC bug by preventing bad installations when 0 <= _Ep
-		// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=68929
-		typedef make_integer_sequence_aux_unchecked<T, 0 <= _Ep ? _Ep : 0> type;
 	};
 
 	template<class T, T _Ep>
@@ -199,5 +155,5 @@ namespace rider::faiz::unroll
 
 	template<class... T>
 	using index_sequence_for = make_index_sequence<sizeof...(T)>;
-} // namespace rider::faiz::unroll
+} // namespace rider::faiz
 #endif
