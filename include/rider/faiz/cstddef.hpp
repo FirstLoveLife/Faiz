cddf369a877706aa6c076f64ed827badcc8ccadd
firstlove <c941968215@gmail.com>
2018-12-02 12:55:59 +0800

    use std::nullptr_t instead

--- include/rider/faiz/cstddef.hpp
+++ include/rider/faiz/cstddef.hpp
@@ -1,69 +1,70 @@
 #ifndef SCTDDEF
-#	define SCTDDEF
-#	include <cstddef>
+#define SCTDDEF
+#include <cstddef>
 namespace rider::faiz
 {
 
 	using size_t = __SIZE_TYPE__;
+	using std::nullptr_t;
 
 	// faiz::ptrdiff_t is the signed integer type of the result of subtracting
 	// two pointers.
-	using ptrdiff_t = __PTRDIFF_TYPE__;
-#	if __cplusplus >= 201103L
-	struct nullptr_t
-	{
-		void* lx;
+	// 	using ptrdiff_t = __PTRDIFF_TYPE__;
+	// #	if __cplusplus >= 201103L
+	// 	struct nullptr_t
+	// 	{
+	// 		void* lx;
 
-		struct nat
-		{
-			int for_bool_;
-		};
-		operator std::nullptr_t()
-		{
-			return nullptr;
-		}
+	// 		struct nat
+	// 		{
+	// 			int for_bool_;
+	// 		};
+	// 		operator std::nullptr_t()
+	// 		{
+	// 			return nullptr;
+	// 		}
 
-		constexpr nullptr_t() : lx(0)
-		{}
-		constexpr nullptr_t(int nat::*) : lx(0)
-		{}
+	// 		constexpr nullptr_t() : lx(0)
+	// 		{}
+	// 		constexpr nullptr_t(int nat::*) : lx(0)
+	// 		{}
 
-		constexpr operator int nat::*() const
-		{
-			return 0;
-		}
+	// 		constexpr operator int nat::*() const
+	// 		{
+	// 			return 0;
+	// 		}
 
-		template<class T>
-		constexpr operator T*() const
-		{
-			return 0;
-		}
+	// 		template<class T>
+	// 		constexpr operator T*() const
+	// 		{
+	// 			return 0;
+	// 		}
 
-		template<class T, class U>
-		operator T U::*() const
-		{
-			return 0;
-		}
+	// 		template<class T, class U>
+	// 		operator T U::*() const
+	// 		{
+	// 			return 0;
+	// 		}
 
-		friend constexpr bool operator==(nullptr_t, nullptr_t)
-		{
-			return true;
-		}
-		friend constexpr bool operator!=(nullptr_t, nullptr_t)
-		{
-			return false;
-		}
-#	else
-	using nullptr_t = decltype(nullptr);
-#	endif
+	// 		friend constexpr bool operator==(nullptr_t, nullptr_t)
+	// 		{
+	// 			return true;
+	// 		}
+	// 		friend constexpr bool operator!=(nullptr_t, nullptr_t)
+	// 		{
+	// 			return false;
+	// 		}
+	// #	else
+	// 	using nullptr_t = decltype(nullptr);
+	// #	endif
 
-	}; // namespace rider::faiz
+	// 	}; // namespace rider::faiz
 
-	inline constexpr nullptr_t
-	get_nullptr_t()
-	{
-		return nullptr_t(0);
-	}
-#	define nullptr rider::faiz::get_nullptr_t()
+	// 	inline constexpr nullptr_t
+	// 	get_nullptr_t()
+	// 	{
+	// 		return nullptr_t(0);
+	// 	}
+	// #	define nullptr rider::faiz::get_nullptr_t()
 } // namespace rider::faiz
 #endif
