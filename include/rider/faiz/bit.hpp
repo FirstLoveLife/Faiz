7f3fdc1f47e3322cf04734740f2bb28ba67f8e66
firstlove <c941968215@gmail.com>
2018-12-16 14:02:54 +0800

    add bit.hpp

---
+++ include/rider/faiz/bit.hpp
@@ -0,0 +1,43 @@
+#ifndef FAIZ_BIT
+#define FAIZ_BIT
+#include "rider/faiz/cstddef.hpp"
+#include <string_view>
+#include <cstdint>
+#include <iostream>
+namespace Rider::Faiz
+{
+	namespace detail
+	{
+		template<char Chs>
+		constexpr int toDecImpl()
+		{
+			return Chs > '9' ? Chs - 'A' + 10 : Chs - '0';
+		}
+	} // namespace detail
+	template<int from, char... Chs>
+	constexpr int
+	toDec()
+	{
+		int ret{};
+
+		return (
+			(ret *= from, ret += detail::toDecImpl<Chs>()), ...);
+	}
+	inline namespace literals
+	{
+		template<char... Chs>
+		constexpr int operator"" _B()
+		{
+			return toDec<2, Chs...>();
+		}
+		template<char... Chs>
+		constexpr int operator"" _O()
+		{
+			return toDec<8, Chs...>();
+		}
+
+		// I decide not to provide heximal, too ugly
+
+	} // namespace literals
+} // namespace Rider::Faiz
+#endif
