3e28bfaa43a21a668ee1dfa43e55fcbcdf3335a4
firstlove <c941968215@gmail.com>
2019-04-18 16:43:36 +0800

    add some functions to "algorithm"

--- include/rider/faiz/algorithm.hpp
+++ include/rider/faiz/algorithm.hpp
@@ -4,23 +4,23 @@
 #include <iostream>
 namespace Rider::Faiz
 {
-	tpl<class T> cexp const T&
+	tpl<typ T> cexp const T&
 	min(const T& a, const T& b)
 	{
 		return (b < a) ? b : a;
 	}
-	tpl<class T> cexp const T&
+	tpl<typ T> cexp const T&
 	max(const T& a, const T& b)
 	{
 		return (b < a) ? a : b;
 	}
 
-	tpl<class T, class Cmp> cexp const T&
+	tpl<typ T, typ Cmp> cexp const T&
 	min(const T& a, const T& b, Cmp less)
 	{
 		return less(b, a) ? b : a;
 	}
-	tpl<class T, class Cmp> cexp const T&
+	tpl<typ T, typ Cmp> cexp const T&
 	max(const T& a, const T& b, Cmp less)
 	{
 		return less(b, a) ? a : b;
@@ -39,8 +39,8 @@ namespace Rider::Faiz
 	// ```
 	// Output:
 	// 1234
-	tpl<class InputIt, class Size, class OutputIt> cexp OutputIt
-	copy_n(InputIt first, Size count, OutputIt result)
+	tpl<typ tInputIt, typ Size, typ OutputIt> cfn
+	copy_n(tInputIt first, Size count, OutputIt result)->OutputIt
 	{
 		if(count > 0)
 		{
@@ -53,15 +53,119 @@ namespace Rider::Faiz
 		return result;
 	}
 
-	tpl<unsigned N, class T> T
-	power(T x)
+	tpl<unsigned N, typ T> cfn
+	power(T x)->T
 	{
-		if
-			cexp(N == 0u) return T(1);
-		else if
-			cexp(N % 2u == 0u) return power<N / 2>(x * x);
+		if(N == 0u)
+			return T(1);
+		else if(N % 2u == 0u)
+			return power<N / 2>(x * x);
 		else
 			return x * power<N - 1>(x);
 	}
+
+	tpl<typ tInputIt1, typ tInputIt2> cfn
+	lexicographical_compare(
+		tInputIt1 first1, tInputIt1 last1, tInputIt2 first2, tInputIt2 last2)
+		->bool
+	{
+		for(; first1 != last1 or first2 != last2; first1++, first2++)
+		{
+			if(*first1 < *first2)
+			{
+				return true;
+			}
+			if(*first1 > *first2)
+			{
+				return false;
+			}
+		}
+		return first1 == last1 && first2 != last2;
+	}
+	tpl<typ tInputIt1, typ tInputIt2, typ tCompare> cfn
+	lexicographical_compare(tInputIt1 first1,
+		tInputIt1 last1,
+		tInputIt2 first2,
+		tInputIt2 last2,
+		tCompare comp)
+		->bool
+	{
+		for(; first1 != last1 or first2 != last2; first1++, first2++)
+		{
+			if(cmp(*first1, *first2))
+			{
+				return true;
+			}
+			if(cmp(*first1, *first2))
+			{
+				return false;
+			}
+		}
+		return first1 == last1 && first2 != last2;
+	}
+
+	tpl<typ tInputIt1, typ tInputIt2> cfn
+	equal(tInputIt1 first1, tInputIt1 last1, tInputIt2 first2)->bool
+	{
+		for(; first1 != last1; first1++, first2++)
+		{
+			if(*first1 != *first2)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	tpl<typ tInputIt1, typ tInputIt2, typ tBinaryPredicate> cfn
+	equal(tInputIt1 first1,
+		tInputIt1 last1,
+		tInputIt2 first2,
+		tBinaryPredicate cmp)
+		->bool
+	{
+		for(; first1 != last1; first1++, first2++)
+		{
+			if(not cmp(*first1, *first2))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	tpl<typ tInputIt1, typ tInputIt2> cfn
+	equal(tInputIt1 first1, tInputIt1 last1, tInputIt2 first2, tInputIt2 last2)
+		->bool
+	{
+		for(; first1 != last1 and first2 != last2; first1++, first2++)
+		{
+			if(*first1 != *first2)
+			{
+				return false;
+			}
+		}
+		return first1 == last1 and first2 == last2;
+	}
+
+
+	tpl<typ tInputIt1, typ tInputIt2, typ tBinaryPredicate> cfn
+	equal(tInputIt1 first1,
+		tInputIt1 last1,
+		tInputIt2 first2,
+		tInputIt2 last2,
+		tBinaryPredicate cmp)
+		->bool
+	{
+		for(; first1 != last1 && first2 != last2; first1++, first2++)
+		{
+			if(not cmp(*first1, *first2))
+			{
+				return false;
+			}
+		}
+		return first1 == last1 and first2 == last2;
+	}
+
 } // namespace Rider::Faiz
 #endif
---
+++ test/algorithm/compare.cpp
@@ -0,0 +1,83 @@
+#include "rider/faiz/algorithm.hpp"
+#include "rider/faiz/container/array.hpp"
+#include <algorithm>
+#include <catch2/catch.hpp>
+
+auto cmp = [](auto x, auto y) { return x + 1 == y; };
+
+TEST_CASE("lexicographical_compare, array", "[lexicographical][array]", )
+{
+	Rider::Faiz::array a1{ 1, 2, 3, 4};
+	// auto a2 = Rider::Faiz::make_array(1, 2, 3, 4);
+	// Rider::Faiz::array a3{1, 2, 3, 4, 5};
+	// Rider::Faiz::array a4{2, 2, 3, 4, 5};
+	// Rider::Faiz::array a5{3, 2, 3, 4};
+	// Rider::Faiz::array a6{7};
+	// REQUIRE(not Rider::Faiz::lexicographical_compare(
+	// 	a1.begin(), a1.end(), a2.begin(), a2.end()));
+	// REQUIRE(Rider::Faiz::lexicographical_compare(
+	// 	a1.begin(), a1.end(), a3.begin(), a3.end()));
+	// REQUIRE(Rider::Faiz::lexicographical_compare(
+	// 	a1.begin(), a1.end(), a4.begin(), a4.end()));
+	// REQUIRE(Rider::Faiz::lexicographical_compare(
+	// 	a1.begin(), a1.end(), a5.begin(), a5.end()));
+	// REQUIRE(Rider::Faiz::lexicographical_compare(
+	// 	a3.begin(), a3.end(), a4.begin(), a4.end()));
+	// REQUIRE(Rider::Faiz::lexicographical_compare(
+	// 	a3.begin(), a3.end(), a5.begin(), a5.end()));
+	// REQUIRE(Rider::Faiz::lexicographical_compare(
+	// 	a3.begin(), a3.end(), a6.begin(), a6.end()));
+}
+
+// TEST_CASE("equal", "[equal][array]", )
+// {
+// 	Rider::Faiz::array a1{1, 2, 3, 4};
+// 	auto a2 = Rider::Faiz::make_array(1, 2, 3, 4);
+// 	Rider::Faiz::array a3{1, 2, 3, 4, 5};
+// 	Rider::Faiz::array a4{2, 2, 3, 4, 5};
+// 	Rider::Faiz::array a5{3, 2, 3, 4};
+// 	Rider::Faiz::array a6{7};
+// 	Rider::Faiz::array a7{2, 3, 4, 5};
+// 	Rider::Faiz::array a8{2, 3, 4, 5, 6};
+// 	REQUIRE(Rider::Faiz::equal(a1.begin(), a1.end(), a2.begin(), a2.end()));
+// 	REQUIRE(
+// 		Rider::Faiz::equal(a1.begin(), a1.end(), a7.begin(), a7.end(), cmp));
+// 	REQUIRE(not Rider::Faiz::equal(
+// 		a1.begin(), a1.end(), a8.begin(), a8.end(), cmp));
+// 	REQUIRE(not Rider::Faiz::equal(a1.begin(), a1.end(), a3.begin(), a3.end()));
+// 	REQUIRE(not Rider::Faiz::equal(a1.begin(), a1.end(), a4.begin(), a4.end()));
+// 	REQUIRE(not Rider::Faiz::equal(a1.begin(), a1.end(), a5.begin(), a5.end()));
+// 	REQUIRE(not Rider::Faiz::equal(a3.begin(), a3.end(), a4.begin(), a4.end()));
+// 	REQUIRE(not Rider::Faiz::equal(a3.begin(), a3.end(), a5.begin(), a5.end()));
+// 	REQUIRE(not Rider::Faiz::equal(a3.begin(), a3.end(), a6.begin(), a6.end()));
+
+// 	REQUIRE(Rider::Faiz::equal(a1.begin(), a1.end(), a2.begin()));
+// 	REQUIRE(Rider::Faiz::equal(a1.begin(), a1.end(), a7.begin(), cmp));
+// 	REQUIRE(Rider::Faiz::equal(a1.begin(), a1.end(), a8.begin(), cmp));
+// 	REQUIRE(Rider::Faiz::equal(a1.begin(), a1.end(), a3.begin()));
+// 	REQUIRE(not Rider::Faiz::equal(a1.begin(), a1.end(), a4.begin()));
+// 	REQUIRE(not Rider::Faiz::equal(a1.begin(), a1.end(), a5.begin()));
+// 	REQUIRE(not Rider::Faiz::equal(a3.begin(), a3.end(), a4.begin()));
+// 	REQUIRE(not Rider::Faiz::equal(a3.begin(), a3.end(), a5.begin()));
+// 	REQUIRE(not Rider::Faiz::equal(a3.begin(), a3.end(), a6.begin()));
+
+// 	REQUIRE(std::equal(a1.begin(), a1.end(), a2.begin(), a2.end()));
+// 	REQUIRE(std::equal(a1.begin(), a1.end(), a7.begin(), a7.end(), cmp));
+// 	REQUIRE(not std::equal(a1.begin(), a1.end(), a8.begin(), a8.end(), cmp));
+// 	REQUIRE(not std::equal(a1.begin(), a1.end(), a3.begin(), a3.end()));
+// 	REQUIRE(not std::equal(a1.begin(), a1.end(), a4.begin(), a4.end()));
+// 	REQUIRE(not std::equal(a1.begin(), a1.end(), a5.begin(), a5.end()));
+// 	REQUIRE(not std::equal(a3.begin(), a3.end(), a4.begin(), a4.end()));
+// 	REQUIRE(not std::equal(a3.begin(), a3.end(), a5.begin(), a5.end()));
+// 	REQUIRE(not std::equal(a3.begin(), a3.end(), a6.begin(), a6.end()));
+
+// 	REQUIRE(std::equal(a1.begin(), a1.end(), a2.begin()));
+// 	REQUIRE(std::equal(a1.begin(), a1.end(), a7.begin(), cmp));
+// 	REQUIRE(std::equal(a1.begin(), a1.end(), a8.begin(), cmp));
+// 	REQUIRE(std::equal(a1.begin(), a1.end(), a3.begin()));
+// 	REQUIRE(not std::equal(a1.begin(), a1.end(), a4.begin()));
+// 	REQUIRE(not std::equal(a1.begin(), a1.end(), a5.begin()));
+// 	REQUIRE(not std::equal(a3.begin(), a3.end(), a4.begin()));
+// 	REQUIRE(not std::equal(a3.begin(), a3.end(), a5.begin()));
+// 	REQUIRE(not std::equal(a3.begin(), a3.end(), a6.begin()));
+// }
