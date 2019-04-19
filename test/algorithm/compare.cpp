232422749af985234c5091b6f5e1f4143bbc881d
firstlove <c941968215@gmail.com>
2019-04-19 11:31:34 +0800

    try to make ci happy

--- test/algorithm/compare.cpp
+++ test/algorithm/compare.cpp
@@ -5,27 +5,28 @@
 
 auto cmp = [](auto x, auto y) { return x + 1 == y; };
 
+Rider::Faiz::array a1{1, 2, 3, 4};
+auto a2 = Rider::Faiz::make_array(1, 2, 3, 4);
+Rider::Faiz::array a3{1, 2, 3, 4, 5};
+Rider::Faiz::array a4{2, 2, 3, 4, 5};
+Rider::Faiz::array a5{3, 2, 3, 4};
+Rider::Faiz::array a6{7};
+
 TEST_CASE("lexicographical_compare, array", "[lexicographical][array]", )
 {
-	Rider::Faiz::array a1{ 1, 2, 3, 4};
-	auto a2 = Rider::Faiz::make_array(1, 2, 3, 4);
-	Rider::Faiz::array a3{1, 2, 3, 4, 5};
-	Rider::Faiz::array a4{2, 2, 3, 4, 5};
-	Rider::Faiz::array a5{3, 2, 3, 4};
-	Rider::Faiz::array a6{7};
-	REQUIRE(not Rider::Faiz::lexicographical_compare(
+	CHECK(not Rider::Faiz::lexicographical_compare(
 		a1.begin(), a1.end(), a2.begin(), a2.end()));
-	REQUIRE(Rider::Faiz::lexicographical_compare(
+	CHECK(Rider::Faiz::lexicographical_compare(
 		a1.begin(), a1.end(), a3.begin(), a3.end()));
-	REQUIRE(Rider::Faiz::lexicographical_compare(
+	CHECK(Rider::Faiz::lexicographical_compare(
 		a1.begin(), a1.end(), a4.begin(), a4.end()));
-	REQUIRE(Rider::Faiz::lexicographical_compare(
+	CHECK(Rider::Faiz::lexicographical_compare(
 		a1.begin(), a1.end(), a5.begin(), a5.end()));
-	REQUIRE(Rider::Faiz::lexicographical_compare(
+	CHECK(Rider::Faiz::lexicographical_compare(
 		a3.begin(), a3.end(), a4.begin(), a4.end()));
-	REQUIRE(Rider::Faiz::lexicographical_compare(
+	CHECK(Rider::Faiz::lexicographical_compare(
 		a3.begin(), a3.end(), a5.begin(), a5.end()));
-	REQUIRE(Rider::Faiz::lexicographical_compare(
+	CHECK(Rider::Faiz::lexicographical_compare(
 		a3.begin(), a3.end(), a6.begin(), a6.end()));
 }
 
